77783d4990ce77baad0b9b91682eb2ef23a95cb4
Christian Niones <149120101+channnyyqt@users.noreply.github.com>
2025-09-05 20:44:30 +0800

    Add files via upload

---
+++ assets/js/model.js
@@ -0,0 +1,13 @@
+class Student {
+  constructor(firstname, middlename, lastname, suffix, contact, email, course, batch, id) {
+    this.firstname = firstname;
+    this.middlename = middlename;
+    this.lastname = lastname;
+    this.suffix = suffix;
+    this.contact = contact;
+    this.email = email;
+    this.course = course;
+    this.batch = batch;
+    this.id = id;
+  }
+}
